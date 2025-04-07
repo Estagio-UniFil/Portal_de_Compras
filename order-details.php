8e59e69de70c88e09ca3e2aa5fb825c7634fd695
Matheus <math@edu.unifil.br>
2025-04-06 23:24:33 -0300

    commit

--- order-details.php
+++ order-details.php
@@ -16,7 +16,7 @@ include('includes/config.php');
 	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
 	    <meta name="robots" content="all">
 
-	    <title>Order History</title>
+	    <title>Histórico de Pedidos</title>
 	    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	    <link rel="stylesheet" href="assets/css/main.css">
 	    <link rel="stylesheet" href="assets/css/red.css">
