89f4660936e54d1585e17b0528677a30393b7369
Matheus Pais de Almeida <math@edu.unifil.br>
2025-06-02 22:48:06 -0300

    Correções

--- pending-orders.php
+++ pending-orders.php
@@ -2,21 +2,6 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-<<<<<<< HEAD
-if(strlen($_SESSION['login'])==0)
-    {   
-header('location:login.php');
-}
-else{
-	if (isset($_GET['id'])) {
-
-		mysqli_query($con,"delete from orders  where userId='".$_SESSION['id']."' and paymentMethod is null and id='".$_GET['id']."' ");
-		
-
-	}
-=======
->>>>>>> 67101b86c78b2e1c87d88d31d6e9bfe402a122b4
-
 if(strlen($_SESSION['login'])==0) {   
     header('location:login.php');
 } else {
